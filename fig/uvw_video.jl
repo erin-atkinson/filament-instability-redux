b4214d1b1e74b05de9f86a0ddde062960a8de281
Erin Atkinson <atkin163@nia-login02.scinet.local>
2025-02-10 13:16:30 -0500

    Velocity component video

--- fig/uvw_video.jl
+++ fig/uvw_video.jl
@@ -18,7 +18,7 @@ function uvw_video(
     colormap = to_colormap(:balance)
     
     fig_kw = (;
-        size=(500, 1000),
+        size=(500, 750),
         fig_kw...
     )
     
@@ -28,15 +28,15 @@ function uvw_video(
     iterations, ts = iterations_times(foldername)
     xsᶜ, xsᶠ, ysᶜ, ysᶠ, zsᶜ, zsᶠ = grid_nodes(foldername)
     
-    filename = joinpath(foldername, "DFM.jl")
+    filename = joinpath(foldername, "DFM.jld2")
     # Find maximum for a constant colorrange
-    u_max = timeseries_of(a->maximum(abs, filt(a, σ)) * L/T, filename, "u_dfm", iterations[frames])
-    v_max = timeseries_of(a->maximum(abs, filt(a, σ)) * L/T, filename, "v_dfm", iterations[frames])
-    w_max = timeseries_of(a->maximum(abs, filt(a, σ)) * L/T, filename, "w_dfm", iterations[frames])
+    u_max = maximum(timeseries_of(a->maximum(abs, filt(a, σ)) * L/T, filename, "u_dfm", iterations[frames]))
+    v_max = maximum(timeseries_of(a->maximum(abs, filt(a, σ)) * L/T, filename, "v_dfm", iterations[frames]))
+    w_max = maximum(timeseries_of(a->maximum(abs, filt(a, σ)) * L/T, filename, "w_dfm", iterations[frames]))
     
-    u_clip = ht_w_kw.colorrange[1] < u_max
-    v_clip = ht_v_kw.colorrange[1] < v_max
-    w_clip = ht_w_kw.colorrange[1] < w_max
+    u_clip = :colorrange in keys(ht_u_kw) && ht_u_kw.colorrange[1] < u_max
+    v_clip = :colorrange in keys(ht_v_kw) && ht_v_kw.colorrange[1] < v_max
+    w_clip = :colorrange in keys(ht_w_kw) && ht_w_kw.colorrange[1] < w_max
     
     frames = let a = 1:length(iterations)
         a[frames]
@@ -46,31 +46,37 @@ function uvw_video(
     t = @lift ts[$frame]
     
     # Set each title
+    d_u_title = :title in keys(ax_u_kw) ? ax_u_kw.title : ""
     u_title = @lift let t_val = round(sp.f * $t / 2π; digits=2)
-        L"%$(ax_u_kw.title) $ft / 2\pi = %$t_val$"
+        L"%$d_u_title $ft / 2\pi = %$t_val$"
     end
+    d_v_title = :title in keys(ax_v_kw) ? ax_v_kw.title : ""
     v_title = @lift let t_val = round(sp.f * $t / 2π; digits=2)
-        L"%$(ax_v_kw.title) $ft / 2\pi = %$t_val$"
+        L"%$d_v_title $ft / 2\pi = %$t_val$"
     end
+    d_w_title = :title in keys(ax_w_kw) ? ax_w_kw.title : ""
     w_title = @lift let t_val = round(sp.f * $t / 2π; digits=2)
-        L"%$(ax_w_kw.title) $ft / 2\pi = %$t_val$"
+        L"%$d_w_title $ft / 2\pi = %$t_val$"
     end
     
     ax_u_kw = (;
         xlabel=L"x/\text{km}",
         ylabel=L"z/\text{m}",
+        limits=(-3*L*sp.L/1000, 3*L*sp.L/1000, -1.2sp.H * L, 0),
         ax_u_kw...,
         title=u_title
     )
     ax_v_kw = (; 
         xlabel=L"x/\text{km}",
         ylabel=L"z/\text{m}",
+        limits=(-3*L*sp.L/1000, 3*L*sp.L/1000, -1.2sp.H * L, 0),
         ax_v_kw...,
         title=v_title
     )
     ax_w_kw = (; 
         xlabel=L"x/\text{km}",
         ylabel=L"z/\text{m}",
+        limits=(-3*L*sp.L/1000, 3*L*sp.L/1000, -1.2sp.H * L, 0),
         ax_w_kw...,
         title=w_title
     )
@@ -78,16 +84,22 @@ function uvw_video(
     ht_u_kw=(;
         colorrange=(-u_max, u_max),
         colormap,
+        highclip= u_clip ? colormap[end] : nothing,
+        lowclip= u_clip ? colormap[1] : nothing,
         ht_u_kw...
     )
     ht_v_kw=(;
         colorrange=(-v_max, v_max),
         colormap,
+        highclip= v_clip ? colormap[end] : nothing,
+        lowclip= v_clip ? colormap[1] : nothing,
         ht_v_kw...
     )
     ht_w_kw=(;
         colorrange=(-w_max, w_max),
         colormap,
+        highclip= w_clip ? colormap[end] : nothing,
+        lowclip= w_clip ? colormap[1] : nothing,
         ht_w_kw...
     )
     ct_b_kw=(;
@@ -103,26 +115,26 @@ function uvw_video(
     
     DFM = jldopen(filename)
     
-    u = @lift get_field(a->filt(a, σ), DFM, "u_dfm", $iteration)
-    v = @lift get_field(a->filt(a, σ), DFM, "v_dfm", $iteration)
-    w = @lift get_field(a->filt(a, σ), DFM, "w_dfm", $iteration)
+    u = @lift get_field(a->filt(a, σ) * L/T, DFM, "u_dfm", $iteration)
+    v = @lift get_field(a->filt(a, σ) * L/T, DFM, "v_dfm", $iteration)
+    w = @lift get_field(a->filt(a, σ) * L/T, DFM, "w_dfm", $iteration)
     b = @lift get_field(a->filt(a, σ), DFM, "b_dfm", $iteration)
     
-    ht_u = heatmap!(ax_u, xsᶠ, zsᶜ, u; ht_u_kw)
-    ht_v = heatmap!(ax_v, xsᶜ, zsᶜ, v; ht_v_kw)
-    ht_w = heatmap!(ax_w, xsᶜ, zsᶠ, w; ht_w_kw)
+    ht_u = heatmap!(ax_u, xsᶠ*L/1000, zsᶜ*L, u; ht_u_kw...)
+    ht_v = heatmap!(ax_v, xsᶜ*L/1000, zsᶜ*L, v; ht_v_kw...)
+    ht_w = heatmap!(ax_w, xsᶜ*L/1000, zsᶠ*L, w; ht_w_kw...)
     
-    contour!(ax_u, xsᶜ, zsᶜ, b; ct_b_kw)
-    contour!(ax_v, xsᶜ, zsᶜ, b; ct_b_kw)
-    contour!(ax_w, xsᶜ, zsᶜ, b; ct_b_kw)
+    contour!(ax_u, xsᶜ*L/1000, zsᶜ*L, b; ct_b_kw...)
+    contour!(ax_v, xsᶜ*L/1000, zsᶜ*L, b; ct_b_kw...)
+    contour!(ax_w, xsᶜ*L/1000, zsᶜ*L, b; ct_b_kw...)
     
     Colorbar(fig[1, 2], ht_u; label=L"u / \text{ms}^{-1}")
     Colorbar(fig[2, 2], ht_v; label=L"v / \text{ms}^{-1}")
     Colorbar(fig[3, 2], ht_w; label=L"w / \text{ms}^{-1}")
     
-    record(fig, output_filename, frames) do i
+    record(fig, output_filename, frames; framerate=12) do i
         frame[] = i
-        print("$(frames[1])->$i->$(frames[end])")
+        print("$(frames[1])->$i->$(frames[end])\r")
     end
     
     close(DFM)
